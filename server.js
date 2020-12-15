9f0408b7aeb5cd48e21b2433be2b5412c21bb47c
Peggy <64912576+pwangy@users.noreply.github.com>
2020-12-15 19:38:57 +0100

    end of video

--- server.js
+++ server.js
@@ -3,49 +3,50 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-// define port for dev
+
+// connect to mongo/mongoose
+const mongoUrl = process.env.MOMGO_URL || "mongodb://localhost/usersExample"
+mongoose.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.Promise = Promise
+
+// define model
+const User = mongoose.model('User', {
+  name: String
+})
+
+// set default port
 const port = process.env.PORT || 8080
 const app = express()
 
-// add middlewares
+// middlewares
 app.use(cors())
 app.use(bodyParser.json())
 
-const mongoURL = process.env.MOMGO_URL || "mongodb://localhost/animals"
-mongoose.connect(mongoURL, {useNewUrlParser: true, useUnifiedTopology: true})
-mongoose.Promise = Promise
-
-const Animal = mongoose.model('Animal', {
-  name: String,
-  age: Number,
-  isFurry: Boolean
+// in order to catch errors and stop things from happening we can use the following. the next() function is the bit that stops things from proceeding.
+// the placement of this bit matters and needs to be BEFORE any route definitions in order to work correctly.
+// this middleware detects if the db is available or not and throws an error if it is unavailable.
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next()
+  } else {
+    res.status(503).json({ error: 'Service unavailable'})
+  }
 })
 
-// we are clearing the db every time we reload the server which is not good practice but helps us in using the following example. the deleteMany.then function which wrapps our animals clears the db. so we only show 3 animals.
-Animal.deleteMany().then(() => {
-  new Animal({ name: 'Alfons', age: 2, isFurry: true }).save()
-  new Animal({ name: 'Lucy', age: 5, isFurry: true }).save()
-  new Animal({ name: 'Goldy da goldfish', age: 1, isFurry: false }).save()
-})
-
-// start defining routes
-app.get('/', (request, response) => {
-  Animal.find().then(animals => {
-    response.json(animals)
-  })
-})
-
-
-// define endpoint to get 1 single animal and its details
-app.get('/:name', (req, res) => {
-  Animal.findOne({name: req.params.name}).then(animal => {
-    if(animal) {
-      res.json(animal)
+// define route
+app.get('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+    if (user) {
+      res.json(user)
     } else {
-      res.status(404).json({ error: 'Not found' })
-    }
-  })
+      res.status(404).json({ error: 'User not found' })
+  }
+ } catch(err) {
+   res.status(400).json({ error: 'Invalid user id' })
+ }
 })
+
 
 // start server
 app.listen(port, () => {
