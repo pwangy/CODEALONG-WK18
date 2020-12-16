5f8a3b6a78d87edf4071f60dbaa1d47c010a579f
Peggy <64912576+pwangy@users.noreply.github.com>
2020-12-16 10:58:37 +0100

    establishing branch

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import mongoose from 'mongoose'
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/books"
+
 import dotenv from 'dotenv'
 
 
@@ -16,10 +20,7 @@ app.use(bodyParser.json())
 
 // define route
 app.get('/', (req, res) => {
-  // fetch('...', { headers: { Authorization: 'super secret key' }})
-  // irl we would never pass the variable to the user, however to show how this works in this example, Damien has the following line to make the point:
-  res.send(process.env.MY_KEY) 
-  // res.send('Hello world')
+  res.send('Hallå!') 
 })
 
 // start server
